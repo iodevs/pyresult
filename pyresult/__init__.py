c9ab6b3f45f5c52bf71cb29cb933fd18637c54dc
Jindrich K. Smitka <jsmitka@smita.info>
2017-05-22 09:51:54 +0200

    Bump version: 0.3.0 → 0.4.0

--- pyresult/__init__.py
+++ pyresult/__init__.py
@@ -20,4 +20,4 @@ from pyresult.operators import (  # noqa
 
 __author__ = """Jindrich Kralevic Smitka"""
 __email__ = 'smitka.j@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='pyresult',
-    version='0.3.0',
+    version='0.4.0',
     description="A Result pattern for python",
     long_description=readme + '\n\n' + history,
     author="Jindrich Kralevic Smitka",
