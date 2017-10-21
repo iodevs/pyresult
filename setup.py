9b64581a126a0ee75be39df5d8ab00942cd35701
Jindrich K. Smitka <jsmitka@smita.info>
2017-10-21 18:39:06 +0200

    Bump version: 0.8.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='pyresult',
-    version='0.8.0',
+    version='1.0.0',
     description="A Result pattern for python",
     long_description=readme + '\n\n' + history,
     author="Jindrich Kralevic Smitka",
